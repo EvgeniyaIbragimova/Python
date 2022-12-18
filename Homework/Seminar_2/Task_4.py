0918f4bdce3f1f19385e11a9e198c19245c3cbe6
Evgeniya Ibragimova <evgeniya_ibragimova@bk.ru>
2022-12-18 19:47:43 +0300

    Homework/Seminar_2/Task_4 done

--- Homework/Seminar_2/Task_4.py
+++ Homework/Seminar_2/Task_4.py
@@ -1,3 +1,14 @@
 # Задайте список из N элементов, заполненных числами из промежутка [-N, N]. 
-# Найдите произведение элементов на указанных позициях. 
-# Позиции хранятся в файле file.txt в одной строке одно число.
+# Найдите произведение элементов на указанных позициях.
+
+N = int(input('Введите число N -> '))
+set = []
+for i in range(-N, N + 1, 1):
+    set.append(i)
+print(f'- Для N = {N}: {set}')
+numbers_index = list(map(int, input('Введите через пробел индексы элементов -> ').split()))
+print(numbers_index)
+product_elements = 1
+for i in numbers_index:
+    product_elements *= set[i]
+print(f'Произведение элементов на указанных позициях -> {product_elements}')
