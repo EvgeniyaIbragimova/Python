4b6738f5ecb59ff008e2f0795a663707d4c53795
Evgeniya Ibragimova <evgeniya_ibragimova@bk.ru>
2022-12-18 19:51:17 +0300

    Homework/Seminar_2/Task_5 done

--- Homework/Seminar_2/Task_5.py
+++ Homework/Seminar_2/Task_5.py
@@ -1,2 +1,14 @@
 # Реализуйте алгоритм перемешивания списка
 # придумать алгоритм как перемешать список
+
+from random import randint
+start_line = int(input('Введите стартовую границу -> '))
+end_line = int(input('Введите конечную границу -> '))
+count_elements = int(input('Введите количество элементов -> '))
+random_numbers = []
+for i in range(count_elements):
+    random_numbers.append(randint(start_line, end_line))
+print(random_numbers)
+import random 
+random.shuffle(random_numbers)
+print(random_numbers)
