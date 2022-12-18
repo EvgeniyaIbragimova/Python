4b41763ee34480a2fad869084ce84e5ae1c20d24
Evgeniya Ibragimova <evgeniya_ibragimova@bk.ru>
2022-12-18 19:26:47 +0300

    Homework/Seminar_2/Task_1 done

--- Homework/Seminar_2/Task_1.py
+++ Homework/Seminar_2/Task_1.py
@@ -2,3 +2,13 @@
 # Пример:
 # - 6782 -> 23
 # - 0,56 -> 11
+
+num = input('Введите вещественное число -> ')
+num_gigits = []
+for i in num:
+    if i.isdigit():
+        num_gigits.append(int(i))
+sum = 0
+for i in num_gigits:
+    sum += i
+print(f'- {num} -> {sum}')
