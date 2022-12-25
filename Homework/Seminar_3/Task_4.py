93ff972f4e44d148f1d8dc2e9f3fa00fc2abe508
Evgeniya Ibragimova <evgeniya_ibragimova@bk.ru>
2022-12-25 11:15:55 +0300

    Homework/Seminar_3/Task_4 done

--- Homework/Seminar_3/Task_4.py
+++ Homework/Seminar_3/Task_4.py
@@ -4,3 +4,10 @@
 # - 3 -> 11
 # - 2 -> 10
 
+decimal_number = int(input('Введите десятичное число -> '))
+binary_number = ''
+temp = decimal_number
+while temp > 0:
+    binary_number = str(temp % 2) + binary_number
+    temp = temp // 2
+print(f'{decimal_number} -> {binary_number}')
