a49609befe50db51ef101ecdfe40811b540e761e
Evgeniya Ibragimova <evgeniya_ibragimova@bk.ru>
2022-12-25 11:37:52 +0300

    Homework/Seminar_3/Task_5 done

--- Homework/Seminar_3/Task_5.py
+++ Homework/Seminar_3/Task_5.py
@@ -3,3 +3,14 @@
 # *Пример:*
 # - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 
+def find_fibanaci(n):
+    fibanaci_list = [0]
+    x, y = 0, 1
+    for i in range(n):
+        x, y = y, x + y
+        fibanaci_list.append(x)
+        fibanaci_list.insert(0, -x if i %2 else x)
+    return fibanaci_list
+
+num = int(input('Введите число -> '))
+print(f'{num} -> {find_fibanaci(num)}')
